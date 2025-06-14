335ab61442f2dde013a5554fcfbb2839df415cb3
DuyunDz23 <tranvan8911@gmail.com>
2025-06-14 12:50:53 +0700

    up

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,25 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: "[BUG]"
+labels: ''
+assignees: ''
+
+---
+
+**Describe the bug**
+A clear and concise description of what the bug is.
+
+**To Reproduce**
+Steps to reproduce the behavior:
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
+
+**Additional information:**
+ - OS: [e.g. Windows 10]
+ - SkidBounce Version [e.g. 0.1.0, or 39a2a05]
--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -54,9 +54,9 @@ object LiquidBounce {
      * This has all of the basic information.
      */
     const val CLIENT_NAME = "SkidBounce"
-    const val CLIENT_AUTHOR = "ManInMyVan"
+    const val CLIENT_AUTHOR = "DuyunDz Skid"
     const val CLIENT_CLOUD = "https://cloud.liquidbounce.net/LiquidBounce"
-    const val CLIENT_WEBSITE = "skidbounce.github.io"
+    const val CLIENT_WEBSITE = "Nohello.en"
 
     val clientVersionText = gitInfo["git.build.version"]?.toString() ?: "unknown"
     val clientCommit = gitInfo["git.commit.id.abbrev"]?.let { "git-$it" } ?: "unknown"
@@ -103,7 +103,6 @@ object LiquidBounce {
             runCatching {
                 async {
                     loadLanguages()
-
                     registerListener(RotationUtils)
                     registerListener(ClientFixes)
                     registerListener(BungeeCordSpoof)
